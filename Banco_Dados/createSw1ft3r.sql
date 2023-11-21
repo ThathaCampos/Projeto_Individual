440c26168caa33a66b345a3f9f6c28c63a09d3f2
ThathaCampos <thamires.campos@sptech.school>
2023-11-20 23:12:09 -0300

    Arrumando o script

--- Banco_Dados/createSw1ft3r.sql
+++ Banco_Dados/createSw1ft3r.sql
@@ -1,6 +1,7 @@
 create database sw1ft3r;
 use sw1ft3r;
 
+
 create table usuario(
 idUsuario int primary key auto_increment,
 nomeCompleto varchar(45),
@@ -11,14 +12,16 @@ senha varchar(45)
 );
 
 select * from usuario;
+SELECT idUsuario, nomeUsuario, email, senha FROM usuario WHERE nomeUsuario = 'thamires@email.com' AND email = '123456' AND senha = 'undefined';
 
 create table questaoCerta(
-idQuestaoCerta int,
+idQuestaoCerta int auto_increment,
 fkUsuario int,
-numeroQuestao int,
+quantCerta int,
 primary key (idQuestaoCerta, fkUsuario),
 constraint fkQuestaoUsuario foreign key (fkUsuario) references usuario(idUsuario)
 );
+select * from questaoCerta;
 
 create table feedback(
 idFeedback int,
