4b5c5ac044db93b657af0ae0d4cb52710a88ec22
ThathaCampos <thamires.campos@sptech.school>
2023-11-24 23:02:09 -0300

    Arrumando a estilização da home

--- Algoritmo/SiteIntegrado/site/public/js/sessao.js
+++ Algoritmo/SiteIntegrado/site/public/js/sessao.js
@@ -1,13 +1,28 @@
 // sessão
 function validarSessao() {
-    var email = sessionStorage.EMAIL_USUARIO;
-    var nome = sessionStorage.NOME_USUARIO;
+    var email = sessionStorage.setItem('email');
+    var nome = sessionStorage.setItem('nomeUsuario');
 
     var b_usuario = document.getElementById("b_usuario");
-
+    
     if (email != null && nome != null) {
         b_usuario.innerHTML = nome;
+        b_usuario.innerHTML = email;
     } else {
+        window.location = "../login.html";
+    }
+}
+
+
+// Validar quantidade de acertos
+function validarAcerto() {
+    var quantidade = sessionStorage.setItem('quantCerta');
+
+    var b_quantidade = document.getElementById("b_quantidade");
+    
+    if(quantidade != null) {
+        b_quantidade.innerHTML = quantidade;
+    } else{
         window.location = "../login.html";
     }
 }
