4bf68348c2dd44954473c4f3326067b6f401cdec
ThathaCampos <thamires.campos@sptech.school>
2023-11-29 23:49:31 -0300

    Inserindo novos selects

--- Banco_Dados/createSw1ft3r.sql
+++ Banco_Dados/createSw1ft3r.sql
@@ -1,75 +1,55 @@
 create database sw1ft3r;
 use sw1ft3r;
 
+
 create table usuario(
 idUsuario int primary key auto_increment,
 nomeCompleto varchar(45),
 dtNasc date,
 email varchar(45),
-nomeUsuario varchar(45),
+nomeUsuario varchar(45) unique,
 senha varchar(45)
 );
-
-
-insert into usuario (idUsuario) values
-	(1),
-    (2),
-    (3),
-    (4);
-    
-insert into usuario (idUsuario, nomeUsuario) values
-	(10, 'Thamires'),
-    (11, 'Vitor'),
-    (12, 'Caique'),
-    (13, 'Bruno');
-
 select * from usuario;
-select fkUsuario, nomeUsuario, max(quantCerta) Quantidade_de_acertos from questaoCerta join usuario on fkUsuario = idUsuario 
-group by fkUsuario, nomeUsuario order by Quantidade_de_acertos and nomeUsuario limit 3;
-
 
 
 create table questaoCerta(
 idQuestaoCerta int auto_increment,
 fkUsuario int,
 quantCerta int,
+dtHora timestamp default current_timestamp,
 primary key (idQuestaoCerta, fkUsuario),
 constraint fkQuestaoUsuario foreign key (fkUsuario) references usuario(idUsuario)
 );
-
-insert into questaoCerta (fkUsuario, quantCerta) values
-	(1, 4),
-    (1, 6),
-    (1, 13),
-    (2, 13),
-    (2, 10),
-    (3, 3),
-    (3, 8);
-
-insert into questaoCerta (fkUsuario, quantCerta) values
-	(2, 7);
-    
-insert into questaoCerta (fkUsuario, quantCerta) values
-	(10, 13),
-    (11, 12),
-    (12, 10),
-    (13, 9);
-
 select * from questaoCerta;
-delete from questaoCerta where idQuestaoCerta in (4, 6, 7, 8, 12,13, 14);
-SELECT quantCerta from questaoCerta where fkUsuario = 1 order by idQuestaoCerta desc limit 1;
-select count(quantCerta) Menor_Que_Cinco,
-	   (select count(quantCerta) from questaoCerta where quantCerta > 5 and quantCerta <= 8) Menor_Que_8,
-       (select count(quantCerta) from questaoCerta where quantCerta > 8) Maior_Que_8
-from questaoCerta
-where quantCerta <= 5;
+
+create table feedback(
+idFeedback int auto_increment,
+fkUsuario int,
+quantEstrelas int,
+dtAvaliacao timestamp default current_timestamp,
+primary key (idFeedback, fkUsuario),
+constraint fkAvaliaUsuario foreign key (fkUsuario) references usuario (idUsuario)
+);
+select * from feedback;
+
+
+
+-- Selects/ Inserts da table usuario
+insert into usuario(nomeCompleto, dtNasc, email, nomeUsuario, senha) values
+('Thamires Campos', '2002-03-29', 'thamires@email.com', 'Thatha', '123456'),
+('Lucas Carvalho', '2004-07-03', 'lucas@email.com', 'Batman', 'batman123'),
+('Bruno Araújo', '2004-08-20', 'bruno@email.com', 'Efacil', 'css123456');
+
+select fkUsuario, nomeUsuario, max(quantCerta) Quantidade_de_acertos from questaoCerta join usuario on fkUsuario = idUsuario 
+group by fkUsuario, nomeUsuario order by Quantidade_de_acertos and nomeUsuario limit 3;
 
 select nomeUsuario,
-	   quantCerta
+	   quantCerta, dtHora
 from questaoCerta
 join usuario on fkUsuario = idUsuario
-where quantCerta >= 0
-order by quantCerta and nomeUsuario desc limit 3;
+where quantCerta >= 0 
+order by dtHora desc;
 
 select nomeUsuario,
 	   (select max(quantCerta) from questaoCerta join usuario on fkUsuario = idUsuario),
@@ -80,14 +60,21 @@ where quantCerta > 8
 order by Quantidade_de_acertos and nomeUsuario;
 
 
-create table feedback(
-idFeedback int auto_increment,
-fkUsuario int,
-quantEstrelas int,
-dtAvaliacao timestamp default current_timestamp,
-primary key (idFeedback, fkUsuario),
-constraint fkAvaliaUsuario foreign key (fkUsuario) references usuario (idUsuario)
-);
+-- Selects/ Inserts da table questaoCerta
+insert into questaoCerta (fkUsuario, quantCerta) values
+(1, 13),
+(2, 8),
+(3, 5);
+select * from questaoCerta;
+delete from questaoCerta where idQuestaoCerta in (4, 6, 7, 8, 12,13, 14);
+SELECT quantCerta from questaoCerta where fkUsuario = 1 order by idQuestaoCerta desc limit 1;
+select count(quantCerta) Menor_Que_Cinco,
+	   (select count(quantCerta) from questaoCerta where quantCerta > 5 and quantCerta <= 8) Menor_Que_8,
+       (select count(quantCerta) from questaoCerta where quantCerta > 8) Maior_Que_8
+from questaoCerta
+where quantCerta <= 5;
+
+-- Selects da table feedback
 select count(quantEstrelas) Igual_1,
 	   (select count(quantEstrelas) from feedback where quantEstrelas = 2) Igual_2,
        (select count(quantEstrelas) from feedback where quantEstrelas = 3) Igual_3,
@@ -96,5 +83,5 @@ select count(quantEstrelas) Igual_1,
 from feedback
 where quantEstrelas = 1;
 
-select * from feedback;
+select quantEstrelas from feedback where fkUsuario = 1;
 
